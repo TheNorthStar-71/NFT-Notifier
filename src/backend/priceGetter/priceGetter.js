21712b1fc3b188a522ce25107ae15411a6b55df7
Robert Lipton <ral325@cornell.edu>
2021-12-24 13:53:16 -0800

    cleaned + added fields returned in the OpenAPI stats object

--- src/backend/priceGetter/priceGetter.js
+++ src/backend/priceGetter/priceGetter.js
@@ -1,13 +1,43 @@
-(async function getCollectionStats(collectionName) {
+async function getCollectionStats(collectionName) {
 
     const options = { method: 'GET', headers: { Accept: 'application/json' } };
 
     let response = await fetch('https://api.opensea.io/api/v1/collection/' + collectionName + '/stats', options)
-        //.then(response => response.json())
-        //.then(response => { return response.stats })
-        //.catch(err => console.error(err));
 
     let data = await response.json();
-    console.log(data.stats)
+
     return data.stats;
+}
+
+/* OpenSea API stats list
+average_price
+count
+floor_price
+market_cap
+num_owners
+num_reports
+one_day_average_price
+one_day_change
+one_day_sales
+one_day_volume
+seven_day_average_price
+seven_day_change
+seven_day_sales
+seven_day_volume
+thirty_day_average_price
+thirty_day_change
+thirty_day_sales
+thirty_day_volume
+total_sales
+total_supply
+total_volume
+*/
+
+
+// --- init test page ///
+let result;
+
+(async() => {
+    result = await getCollectionStats("cryptopunks");
+    console.log(result)
 })();
